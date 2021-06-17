a10b3dd65b00950d23bb8450edb4f92a33f2da69
D.A.N_3002 <dinhanh300229@gmail.com>
2021-06-17 13:45:43 +0700

    [FEAT] Show Date and Weather data

---
+++ DIY/Lessons/AnhND_Lesson18/public/scripts/Weather.js
@@ -0,0 +1,34 @@
+const API_KEY = '74c23c2d9a83caffe161458d751d26a2';
+
+const Weather = {
+	async getWeatherData(){
+		const location = await Location.getCurrentLocation();
+		const {latitude, longitude} = location;
+  
+		const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+		const  weatherData = await $.get(apiURL);
+  
+		let temp = Math.round(weatherData.main.temp - 273.15),
+		   {name} = weatherData,
+		   weather = weatherData.weather[0];
+		let iconURL = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+  
+		return {
+		   temp, name, weather, iconURL
+		}
+	},
+
+	async init() {
+		const weatherData = await Weather.getWeatherData();
+
+		console.log(weatherData);
+
+		$(".location").text(weatherData.name);
+		$(".degree > .num").html(weatherData.temp  + "<sup>o</sup>C");
+		$("#weather-icon").attr("src", weatherData.iconURL);
+	}
+}
+
+$(document).ready(function () {
+	Weather.init();
+});
