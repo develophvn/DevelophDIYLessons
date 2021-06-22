afb6e1657bd2c331a0c92b1814bb8fc45f34664e
D.A.N_3002 <dinhanh300229@gmail.com>
2021-06-22 21:49:10 +0700

    [FEATURE] Get todos data

--- DIY/Lessons/AnhND_Lesson20/app/ui/router.js
+++ DIY/Lessons/AnhND_Lesson20/app/ui/router.js
@@ -3,16 +3,21 @@ import express from 'express';
 import readData from '../news/lib/readData.js';
 import processData from '../news/lib/processData.js';
 import { NEWS_LIMIT } from '../config.js';
+import { Todos } from '../models/index.js';
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
 	const xmlData = await readData();
 
-	const newsData = await processData(xmlData);
-	newsData.length = NEWS_LIMIT;
+	const newsData = await processData(xmlData);	
+	const todos = await Todos.find({});
 
-	res.render('dashboard', { newsData });
+	if (newsData.length > NEWS_LIMIT) {
+		newsData.length = NEWS_LIMIT;
+	}
+
+	res.render('dashboard', { newsData, todos });
 });
 
 export default router;
