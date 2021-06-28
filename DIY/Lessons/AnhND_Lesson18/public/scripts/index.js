94bdb88ee2f4c3d847fb308ae5434ced037e041e
D.A.N_3002 <dinhanh300229@gmail.com>
2021-06-29 00:17:05 +0700

    [FIX] Change Jquery into VanillaJS

--- DIY/Lessons/AnhND_Lesson18/public/scripts/index.js
+++ DIY/Lessons/AnhND_Lesson18/public/scripts/index.js
@@ -3,9 +3,11 @@ function initDate() {
 	const dateTimeFormat = new Intl.DateTimeFormat('en', { year: 'numeric', month: 'short', day: '2-digit' })
 	const [{ value: month },,{ value: day },,{ value: year }] = dateTimeFormat.formatToParts(date)
  
-	$("#current-date").text(`${day}/${month}/${year}`);
+
+	const dateElement = document.getElementById('current-date');
+	dateElement.innerHTML = `${day}/${month}/${year}`;
 }
 
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function(e) {
 	initDate();
-});
+})
